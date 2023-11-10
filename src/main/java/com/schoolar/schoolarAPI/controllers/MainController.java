68f7fd368309a583369865f53ba4a80fa07547c1
VitorMantovani <mantovanifreitasvitor@gmail.com>
2023-11-10 19:20:30 -0300

    feat: adds route

---
+++ src/main/java/com/schoolar/schoolarAPI/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.schoolar.schoolarAPI.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping()
+    public String davidLindo() { return "Hello World"; }
+}
